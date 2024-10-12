c32c96d55864aa5559635e8cc430c62ef40792ae
Emile Drablant <emiledrablant@proton.me>
2024-10-12 10:48:25 -0300

    add internal logic for a round

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let number = Math.floor(Math.random() * 3);
     if (number === 0) {
@@ -10,11 +13,35 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let choice = prompt("Please choose between rock, paper or scissors.");
+    let choice = prompt("Please choose between rock, paper or scissors.").toLowerCase();
     if (choice === "rock" || choice === "paper" || choice === "scissors") {
         return choice;
     } else {
         return undefined;
+    }
+}
+
+function playRound(computerChoice, humanChoice) {
+    if (computerChoice === "rock" && humanChoice === "paper") {
+        console.log("You win: paper beats rock");
+        humanScore += 1;
+    } else if (computerChoice === "paper" && humanChoice === "rock") {
+        console.log("You lose: paper beats rock");
+        computerScore += 1;
+    } else if (computerChoice === "paper" && humanChoice === "scissors") {
+        console.log("You win: scissors beats paper");
+        humanScore += 1;
+    } else if (computerChoice === "scissors" && humanChoice === "paper") {
+        console.log("You lose: scissors beats paper");
+        computerScore += 1;
+    } else if (computerChoice === "scissors" && humanChoice === "rock") {
+        console.log("You win: rock beats scissors");
+        humanScore += 1;
+    } else if (computerChoice === "rock" && humanChoice === "scissors") {
+        console.log("You lose: scissors beats rock");
+        computerScore += 1;
+    } else {
+        console.log("You both choose the same thing. It's a tie.")
     }
 }
 
@@ -25,4 +52,6 @@ while (choice === undefined) {
    choice = getHumanChoice();
 }
 
-console.log("computeur: " + computerChoice + " you : " + choice);
+playRound(computerChoice, choice);
+
+// console.log("computeur: " + computerChoice + " you : " + choice);
